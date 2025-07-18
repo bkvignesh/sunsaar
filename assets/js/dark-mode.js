242e91cfbb6d6bf8f46a1dffeca994b34364334b
BK Vignesh <66754882+bkvignesh@users.noreply.github.com>
2025-07-18 09:42:16 +0000

    changes to dark mode toggle

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,10 +1,20 @@
 const toggle = document.getElementById('mode-toggle');
-const root   = document.documentElement;
+const root = document.documentElement;
 
-// Load choice
-if (localStorage.theme === 'dark') root.classList.add('dark');
+// Set theme on load
+function setThemeOnLoad() {
+  if (localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    root.classList.add('dark');
+  } else {
+    root.classList.remove('dark');
+  }
+}
+setThemeOnLoad();
 
-toggle.onclick = () => {
-  root.classList.toggle('dark');
-  localStorage.theme = root.classList.contains('dark') ? 'dark' : 'light';
-};
+if (toggle) {
+  toggle.onclick = () => {
+    root.classList.toggle('dark');
+    localStorage.theme = root.classList.contains('dark') ? 'dark' : 'light';
+  };
+}
